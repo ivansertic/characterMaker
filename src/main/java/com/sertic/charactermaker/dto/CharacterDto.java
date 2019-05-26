ffc867d19e0884a72a53c75dcc406a796e15c881
ivansertic <serticivan5@gmail.com>
2019-05-26 12:05:52 +0200

    Added Sets For CharacterDto

--- src/main/java/com/sertic/charactermaker/dto/CharacterDto.java
+++ src/main/java/com/sertic/charactermaker/dto/CharacterDto.java
@@ -1,5 +1,7 @@
 package com.sertic.charactermaker.dto;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public class CharacterDto {
@@ -16,6 +18,111 @@ public class CharacterDto {
 
     private UserDto userDto;
 
-    /*Need to implement sets*/
+    private CharacterStatisticDto characterStatisticDto;
 
+    private Set<CoinDto> coinDtoSet = new HashSet<>();
+
+    private Set<ItemDto> itemDtoSet = new HashSet<>();
+
+    private Set<SkillDto> skillDtoSet = new HashSet<>();
+
+    private Set <SpellDto> spellDtoSet = new HashSet<>();
+
+    private Set <WeaponDto> weaponDtoSet = new HashSet<>();
+
+    public Long getIdDto() {
+        return idDto;
+    }
+
+    public void setIdDto(Long idDto) {
+        this.idDto = idDto;
+    }
+
+    public UUID getExternalCharacterIdDto() {
+        return externalCharacterIdDto;
+    }
+
+    public void setExternalCharacterIdDto(UUID externalCharacterIdDto) {
+        this.externalCharacterIdDto = externalCharacterIdDto;
+    }
+
+    public String getNameDto() {
+        return nameDto;
+    }
+
+    public void setNameDto(String nameDto) {
+        this.nameDto = nameDto;
+    }
+
+    public String getCharacterClassDto() {
+        return characterClassDto;
+    }
+
+    public void setCharacterClassDto(String characterClassDto) {
+        this.characterClassDto = characterClassDto;
+    }
+
+    public String getRaceDto() {
+        return raceDto;
+    }
+
+    public void setRaceDto(String raceDto) {
+        this.raceDto = raceDto;
+    }
+
+    public UserDto getUserDto() {
+        return userDto;
+    }
+
+    public void setUserDto(UserDto userDto) {
+        this.userDto = userDto;
+    }
+
+    public CharacterStatisticDto getCharacterStatisticDto() {
+        return characterStatisticDto;
+    }
+
+    public void setCharacterStatisticDto(CharacterStatisticDto characterStatisticDto) {
+        this.characterStatisticDto = characterStatisticDto;
+    }
+
+    public Set<CoinDto> getCoinDtoSet() {
+        return coinDtoSet;
+    }
+
+    public void setCoinDtoSet(Set<CoinDto> coinDtoSet) {
+        this.coinDtoSet = coinDtoSet;
+    }
+
+    public Set<ItemDto> getItemDtoSet() {
+        return itemDtoSet;
+    }
+
+    public void setItemDtoSet(Set<ItemDto> itemDtoSet) {
+        this.itemDtoSet = itemDtoSet;
+    }
+
+    public Set<SkillDto> getSkillDtoSet() {
+        return skillDtoSet;
+    }
+
+    public void setSkillDtoSet(Set<SkillDto> skillDtoSet) {
+        this.skillDtoSet = skillDtoSet;
+    }
+
+    public Set<SpellDto> getSpellDtoSet() {
+        return spellDtoSet;
+    }
+
+    public void setSpellDtoSet(Set<SpellDto> spellDtoSet) {
+        this.spellDtoSet = spellDtoSet;
+    }
+
+    public Set<WeaponDto> getWeaponDtoSet() {
+        return weaponDtoSet;
+    }
+
+    public void setWeaponDtoSet(Set<WeaponDto> weaponDtoSet) {
+        this.weaponDtoSet = weaponDtoSet;
+    }
 }
